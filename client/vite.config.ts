ba8f154b09c9d9079207a589184d613edfb2350c
Hoàng Anh <legendarycaptains@gmail.com>
2024-10-22 22:21:46 +0700

    (#10) Update codebase

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv, ProxyOptions } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Config proxy for development mode
+const getProxyOptions = (mode: string) => {
+  if (mode === "development") {
+    return {
+      "/api": {
+        target: "BACKEND_URL",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    } as Record<string, ProxyOptions>;
+  }
+};
+
+export default ({ mode }: { mode: string }) => {
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [react(), checker({ typescript: true })],
+    base: "",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    server: {
+      port: Number(process.env.PORT) || 9000,
+      open: true,
+      proxy: getProxyOptions(mode),
+    },
+    define: {
+      "process.env": { ...loadEnv(mode, process.cwd(), "ENV_") },
+    },
+    publicDir: "public",
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          app: "./index.html",
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
+        },
+      },
+    },
+  });
+};
+
